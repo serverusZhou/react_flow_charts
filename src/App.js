09c2b1593fa6349f49b73986ea66658d9934fe86
arvin.zhou <serverus@sina.com>
2018-08-03 09:53:52 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types';
+
+class Chart extends Component{
+    render(){
+        return (
+            <div>
+                <aside>左边组件部分</aside>
+            </div>
+        )
+    }
+}
+
+
+
+export default Chart
